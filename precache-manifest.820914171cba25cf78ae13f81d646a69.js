33fcbc99d4fd9cdc59c727178993395dac6edab2
Samuel Madigage <samlambo929@gmail.com>
2019-04-23 14:53:32 +0200

    Updates

---
+++ precache-manifest.820914171cba25cf78ae13f81d646a69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "d524b4bf7edaf38d74ea",
+    "url": "/contactmanager/static/js/main.c131c656.chunk.js"
+  },
+  {
+    "revision": "6c71d638a0c2e6c39b4d",
+    "url": "/contactmanager/static/js/2.4f39fd69.chunk.js"
+  },
+  {
+    "revision": "d524b4bf7edaf38d74ea",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "6c71d638a0c2e6c39b4d",
+    "url": "/contactmanager/static/css/2.a663e9fc.chunk.css"
+  },
+  {
+    "revision": "78a390c29047c5d6b4e7ddaf68a7888f",
+    "url": "/contactmanager/index.html"
+  }
+];
